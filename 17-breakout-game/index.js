66f412afa49815a26dbf240e7643184938d90d18
sincerity628 <14785481094@163.com>
2020-02-21 14:12:18 +0800

    added the ball, paddle, score drawing in the canvas.

--- 17-breakout-game/index.js
+++ 17-breakout-game/index.js
@@ -1,6 +1,64 @@
 const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+// set the ball props
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  dx: 4,
+  dy: 4
+};
+
+// set the paddle props
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  dx: 8
+};
+
+// the total score
+let score = 0;
+
+// draw the ball on the canvas
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = '#745c97';
+  ctx.fill();
+  ctx.closePath();
+}
+
+// draw the paddle on the canvas
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.fillRect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = '#745c97';
+  ctx.fill();
+  ctx.closePath();
+}
+
+// draw the score on the canvas
+function drawScore() {
+  ctx.beginPath();
+  ctx.font = '20px Arial';
+  ctx.fillText(`Score: ${ score }`, canvas.width - 100, 40);
+  ctx.closePath();
+}
+
+// draw everything on the canvas
+function draw() {
+  drawBall();
+  drawPaddle();
+  drawScore();
+}
+
+draw();
 
 // event listener
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
