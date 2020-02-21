de4a076f3db91385730d8928920d920b12803c74
sincerity628 <14785481094@163.com>
2020-02-21 14:41:18 +0800

    added the bricks drawing on the canvas.

--- 17-breakout-game/index.js
+++ 17-breakout-game/index.js
@@ -9,6 +9,7 @@ const ball = {
   x: canvas.width / 2,
   y: canvas.height / 2,
   size: 10,
+  speed: 4,
   dx: 4,
   dy: 4
 };
@@ -19,11 +20,36 @@ const paddle = {
   y: canvas.height - 20,
   w: 80,
   h: 10,
-  dx: 8
+  speed: 8,
+  dx: 0
 };
 
 // the total score
 let score = 0;
+
+// set the brick props
+const brickInfo = {
+  w: 70,
+  h: 20,
+  offsetX: 45,
+  offsetY: 60,
+  padding: 10,
+  visible: true
+};
+
+// create the bricks
+const brickColumnCount = 9;
+const brickRowCount = 5;
+let bricks = [];
+
+for(let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for(let j = 0; j < brickColumnCount; j++) {
+    const x = j * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = i * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
 
 // draw the ball on the canvas
 function drawBall() {
@@ -51,11 +77,25 @@ function drawScore() {
   ctx.closePath();
 }
 
+// draw the bricks on the canvas
+function drawBricks() {
+  bricks.forEach(row => {
+    row.forEach(brick => {
+      ctx.beginPath();
+      ctx.fillRect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? '#745c97' : 'transparent';
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+}
+
 // draw everything on the canvas
 function draw() {
   drawBall();
   drawPaddle();
   drawScore();
+  drawBricks();
 }
 
 draw();
